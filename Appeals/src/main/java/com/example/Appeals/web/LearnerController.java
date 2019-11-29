193526fabe0730e95addd22f1ae6d760e21a3d96
Basel0348 <basel0348@gmail.com>
2019-11-29 19:29:18 +0200

    fix learners api

--- Appeals/src/main/java/com/example/Appeals/web/LearnerController.java
+++ Appeals/src/main/java/com/example/Appeals/web/LearnerController.java
@@ -2,12 +2,14 @@ package com.example.Appeals.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Appeals.domin.Learner;
 import com.example.Appeals.domin.LearnerRepository;
 import com.example.Appeals.domin.Question;
 import com.example.Appeals.domin.QuestionRepository;
 
+@RestController
 public class LearnerController {
 	@Autowired
     private LearnerRepository learnerrepository;
